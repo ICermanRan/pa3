4f4443dde89e6d1ef042c3d944eff5f71a85e462
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-23 19:09:11 +0800

    > compile NEMU ysyx_22050078 李秋然 Linux ran 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:09:11 up  2:04,  1 user,  load average: 0.74, 0.48, 0.33

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,47 @@
+#include "verilated_vcd_c.h" //添加VCD头文件
+#include "Vtop.h"  //这里因为新建的rtl文件为top.c，所以需要改为Vtop.h即可。
+#include "verilated.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 
-int main() {
-  printf("Hello, ysyx!\n");
+vluint64_t main_time = 0;  //initial 仿真时间
+
+double sc_time_stamp()
+{
+	return main_time;
+}
+
+int main(int argc, char** argv, char** env) {
+  VerilatedContext* contextp = new VerilatedContext;
+  contextp->commandArgs(argc, argv);
+  Vtop* top = new Vtop{contextp};
+	//VCD波形设置  start
+  Verilated::traceEverOn(true);
+  VerilatedVcdC* tfp = new VerilatedVcdC;
+  top->trace(tfp, 0);
+  tfp->open("wave.vcd");
+	//VCD波形设置  end
+  while (sc_time_stamp() < 20 && !contextp->gotFinish()) { 
+    int a = rand() & 1;
+    int b = rand() & 1;
+    top->a = a;
+    top->b = b;
+    top->eval();
+	printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+	//contextp->timeInc(1);
+	tfp->dump(main_time);
+	main_time++;
+    assert(top->f == (a ^ b));
+  }
+  delete top;
+  tfp->close();	
+  delete contextp;
   return 0;
 }
+
+
+
+
+
+
