571bb494e72060821182e8da018a5fb640dc933a
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-28 09:37:20 +0800

    > compile NEMU ysyx_22050078 李秋然 Linux ran 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:37:20 up  1:49,  1 user,  load average: 0.65, 0.55, 0.54

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -92,15 +92,18 @@ static int cmd_info(char *args) {
 
 static int cmd_x(char *args) {
 	/* extract the first argument */
-
+  char *arg = NULL; //定义分割出来的每个字符串
+  arg = strtok(args, " ");// 在第一次分割时，需要指定源字符串
 	
-  char *arg;
-  arg = strtok(args, " ");
-	printf("%s\n", arg);
+  while (arg != 0)
+  {
+    printf("%s\n", arg);
+    arg = strtok(NULL, ",");  // 之后的每一次分割只需将第一个参数换成NULL即可
+  }
+  
+  
 
- /* char *addr = strtok(arg, " ");
-	printf("%s\n", addr);
-*/
+
 
   return 0;
 }
