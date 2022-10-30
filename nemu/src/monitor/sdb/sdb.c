303ee496ed565d5709096196e8de53f61d35501a
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-30 15:57:20 +0800

    > compile NEMU ysyx_22050078 李秋然 Linux ran 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:57:20 up 34 min,  1 user,  load average: 0.42, 0.40, 0.37

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -98,7 +98,7 @@ static int cmd_x(char *args) {
   int i;
   long long base_addr;
   int j;
-  uint64_t value;
+  long long value;
   uint8_t word_size = 0x00000008;
  
   arg = strtok(args, " ");// 在第一次分割时，需要指定源字符串
@@ -151,7 +151,7 @@ static int cmd_x(char *args) {
   for(j = 0; j < i; j++)
   {
     value = vaddr_read(base_addr + j*word_size, word_size);
-    printf("%ld\n", value);
+    printf("%#llx\n", value);
   }
  
 
