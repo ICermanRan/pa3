9b9acff7d6d57f2576e54bdae2999e446f268878
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-30 10:28:28 +0800

    > compile NEMU ysyx_22050078 李秋然 Linux ran 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:28:28 up  1:46,  1 user,  load average: 0.26, 0.33, 0.47

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -96,7 +96,7 @@ static int cmd_x(char *args) {
   char *arg1 = NULL;
 
   int i;
- // int base_addr;
+  int base_addr;
  // int value,j;
  // uint8_t word_size = 0x00000008;
  
@@ -128,7 +128,7 @@ static int cmd_x(char *args) {
     } ////用ASCII码值做计算，得到的值正好为对应的数字
 
     
- /* 
+  
   long m = 1;
   long long sum = 0; 
   int c;
@@ -140,7 +140,7 @@ static int cmd_x(char *args) {
 
    base_addr = sum;//分离出起始地址int类型
    printf("%#x\n", base_addr);
-
+/*
   for(j = 0; j < i; j++)
   {
     value = vaddr_read(base_addr + j*word_size, word_size);
