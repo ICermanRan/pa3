2e337713d90719d3080f5aa25f71bbe891050234
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-30 15:40:19 +0800

    > compile NEMU ysyx_22050078 李秋然 Linux ran 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:40:19 up 17 min,  1 user,  load average: 0.68, 0.56, 0.37

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -124,7 +124,7 @@ static int cmd_x(char *args) {
   //int y = strlen(arr);
   //printf("y = %d\n", y);
   int num[a];//数组存储数量=y-2
-    for(b = 2; b <= a; b++)
+    for(b = 2; b <= a-1; b++)
     {
         if((arr[b] >= 48) && (arr[b] <= 57))
           num[b-2] = arr[b] - '0';
