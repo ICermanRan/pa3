1953acb9cd37bd1e8a7c080f4a44f4bb35e45a9f
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-01 16:53:33 +0800

    > compile NEMU ysyx_22050078 李秋然 Linux ran 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:53:33 up  9:28,  1 user,  load average: 0.82, 1.08, 0.83

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -80,7 +80,7 @@ static bool make_token(char *e) {
   int i;
   regmatch_t pmatch;//存放匹配文本串位置信息
 
-  puts(e);
+  printf("e = %s\n", e);
   nr_token = 0;
   while (e[position] != '\0') {
     /* Try all rules one by one. */
