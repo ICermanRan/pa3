86d7be9a0ae42daa56b375cd41551728aaf3f5fc
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-01 11:27:35 +0800

    > compile NEMU ysyx_22050078 李秋然 Linux ran 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:27:35 up  4:02,  1 user,  load average: 0.60, 0.51, 0.48

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -95,7 +95,9 @@ static bool make_token(char *e) {
         printf("i = %d, rules[i] = %s\n",  i, rules[i].regex);
         nr_token++;
         printf("nr_token = %d\n", nr_token);
+
         position += substr_len;
+        printf("position = %d\n", position);
 
         /* TODO: Now a new token is recognized with rules[i]. Add codes
          * to record the token in the array `tokens'. For certain types
