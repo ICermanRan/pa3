d7e7d725f0a2f4b59d601bc287ee2753397959c2
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-02 18:29:38 +0800

    > compile NEMU ysyx_22050078 李秋然 Linux ran 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:29:38 up 11:02,  1 user,  load average: 0.56, 0.42, 0.44

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -98,11 +98,11 @@ static bool make_token(char *e) {
         Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
             i, rules[i].regex, position, substr_len, substr_len, substr_start);
         
-        printf("match char = %.*s\n", substr_len,substr_start);
+        // printf("match char = %.*s\n", substr_len,substr_start);
 
       //  printf("i = %d, rules[i] = %s\n",  i, rules[i].regex);
         nr_token++; //每次匹配成功一个字符就自加1
-        printf("nr_token = %d\n", nr_token);
+        // printf("nr_token = %d\n", nr_token);
 
         position += substr_len;
       //  printf("position = %d\n", position);
