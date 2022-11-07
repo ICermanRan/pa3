42a2031f3e97a025515f1f4f2344d2fdf23869c8
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-07 09:25:25 +0800

    > compile NEMU ysyx_22050078 李秋然 Linux ran 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:25:25 up  1:54,  1 user,  load average: 0.29, 0.23, 0.30

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -237,8 +237,8 @@ static int check_parentheses(int p, int q)
   int i;
   int cnt_l = 0,cnt_r = 0;//左右括号计数器
 
-  printf("进入括号配对函数\n");
-  printf("传入的p = %d ,传入的q = %d\n", p, q);
+  // printf("进入括号配对函数\n");
+  // printf("传入的p = %d ,传入的q = %d\n", p, q);
   for(i = p; i <= q; i++)
     {
       if((tokens[i].type == '('))
@@ -249,7 +249,7 @@ static int check_parentheses(int p, int q)
       // printf("cnt_l = %d, cnt_r = %d\n", cnt_l,cnt_r);
       if(cnt_l < cnt_r)
       {
-        printf("括号存在不配对,程序中止");
+        // printf("括号存在不配对,程序中止");
         return condition_2 = 0;
       }
     }
@@ -274,8 +274,8 @@ static bool check_surround(int p, int q)
   
   a = tokens[p].type;
   b = tokens[q].type;
-  printf("进入括号包围函数\n");
-  printf("传入的p = %d ,传入的q = %d\n", p, q);
+  // printf("进入括号包围函数\n");
+  // printf("传入的p = %d ,传入的q = %d\n", p, q);
  // printf("%s = %d, %s = %d\n", tokens[p].str, a, tokens[q].str,b);
  
   if((a == '(') && (b == ')')) //判断是否被一对匹配的括号包围
@@ -286,14 +286,14 @@ static bool check_surround(int p, int q)
   
   if(condition_1 == 1)
   {
-    printf("condition_1 = %d\n", condition_1);
-    printf("被括号包围，返回true\n");
+    // printf("condition_1 = %d\n", condition_1);
+    // printf("被括号包围，返回true\n");
     logic1 = true;
   }
    if(condition_1 == 0)
   {
-    printf("condition_1 = %d\n", condition_1);
-    printf("未被括号包围，返回false\n");
+    // printf("condition_1 = %d\n", condition_1);
+    // printf("未被括号包围，返回false\n");
     logic1 = false;
   }
     
@@ -312,7 +312,7 @@ static bool check_surround(int p, int q)
   int stop_1 = 0,stop_2 = 0;
   int main_addr = 0;
  
-  printf("进入main_op，p = %d, q = %d\n", p ,q);
+  // printf("进入main_op，p = %d, q = %d\n", p ,q);
 
   for(j = q; j >= p; j--)
   {
@@ -399,10 +399,9 @@ static int eval(int start, int end)  //p=开始位置，q=结束位置
   
   p = start;
   q = end;
-  //printf("token_addrs = %d, q = %d\n", token_addrs, q);
-  //op = main_op(token_addrs);//返回的是op在tokens数组中的位置
-  printf("进入eval,p = %d, q = %d\n", p, q);
-  //check_parentheses(p, q);
+  
+  // printf("进入eval,p = %d, q = %d\n", p, q);
+
 
   if (p > q)
    {
@@ -412,9 +411,9 @@ static int eval(int start, int end)  //p=开始位置，q=结束位置
 
   else if (p == q)
    {
-    printf("2、判断为:It's a number\n"); 
+    // printf("2、判断为:It's a number\n"); 
     value_num = atoi(tokens[p].str); 
-    printf("value_num = %d\n" , value_num);
+    // printf("value_num = %d\n" , value_num);
     //return result = value_num;   
      result = value_num;
      return result;
@@ -428,18 +427,18 @@ static int eval(int start, int end)  //p=开始位置，q=结束位置
 
   else if ((check_surround(p, q) == true) && (check_parentheses(p,q) == 1))//被包围且匹配
   {
-    printf("begin to solve 括号\n");
+    // printf("begin to solve 括号\n");
     
     if((check_parentheses(p+1,q-1) == 1) && (check_surround(p+1,q-1) == false))
     {
-      printf("去掉外面一层括号后，不再被括号包围且内部括号配对：\n");
+      // printf("去掉外面一层括号后，不再被括号包围且内部括号配对：\n");
       result = eval(p + 1, q - 1);
       return result;
     }
     else if((check_parentheses(p+1,q-1) == 0) && (check_surround(p+1,q-1) == false))
     {
-      printf("去掉外面一层括号后，不再被括号包围但内部括号不配对：\n");
-      printf("此时先找主运算符");
+      // printf("去掉外面一层括号后，不再被括号包围但内部括号不配对：\n");
+      // printf("此时先找主运算符");
       op = main_op(p,q);
       op_type = tokens[op].type;
       //printf("在去掉两边括号不对时，就先找主运算符,the position of 主运算符op = %s in the token expression: %d\n", tokens[op].str, op);
@@ -466,8 +465,8 @@ static int eval(int start, int end)  //p=开始位置，q=结束位置
    // printf("针对去掉括号没有问题的时候：\n");
     op = main_op(p,q);
     op_type = tokens[op].type;
-    printf("找主运算符,the position of 主运算符op = %s in the token expression: %d\n", tokens[op].str, op);
-    printf("开始求值\n");
+    // printf("找主运算符,the position of 主运算符op = %s in the token expression: %d\n", tokens[op].str, op);
+    // printf("开始求值\n");
     val1 = eval(p, op - 1);
     val2 = eval(op + 1, q);
       switch (op_type) {
