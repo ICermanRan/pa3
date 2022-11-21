fe0d0a0ef578737745bad214001d6313015937a9
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-21 19:36:56 +0800

    > compile NEMU ysyx_22050078 李秋然 Linux ran 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:36:56 up  8:18,  1 user,  load average: 1.14, 0.92, 0.80

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -33,46 +33,46 @@ int main(int argc, char *argv[]) {
 
   /* Start engine. */
   /********测试表达式求值-start********/
-  	FILE *fp;
-  	fp = fopen("/home/ran/ysyx/ysyx-workbench/nemu/tools/gen-expr//build/input", "r");
-  	if(fp == NULL)
-	 {
-		printf("cannot open this file\n");
-   		return 0;
-	 }
+  	// FILE *fp;
+  	// fp = fopen("/home/ran/ysyx/ysyx-workbench/nemu/tools/gen-expr//build/input", "r");
+  	// if(fp == NULL)
+	//  {
+	// 	printf("cannot open this file\n");
+   	// 	return 0;
+	//  }
 	
-  	_Bool * success = NULL;
-  	unsigned long result;
-	char buf[65536] = {};
-	char dtm[65536];
-	char expr_2[65536];
-	char *arg = NULL; 
-	char *expr_wrong = NULL;
-	char *expr_ready = NULL;
+  	// _Bool * success = NULL;
+  	// unsigned long result;
+	// char buf[65536] = {};
+	// char dtm[65536];
+	// char expr_2[65536];
+	// char *arg = NULL; 
+	// char *expr_wrong = NULL;
+	// char *expr_ready = NULL;
 
-	 while (fgets(buf, sizeof(buf), fp) != NULL)
-    {
-	  strcpy(dtm,buf);//把 buf 所指向的字符串复制到dtm。
-	  arg = strtok(dtm, " ");// 在第一次分割时，需要指定源字符串
-  	  if(arg != NULL)
-      {
-		sscanf(dtm,"%lu", &result);//sscanf有局限性，遇到空格就结束了，所以只用于读取result
-   	  	expr_wrong = strtok(NULL, "\0");//往后的调用则将参数s设置成NULL
-		int strLen = strlen(expr_wrong);
-	    // printf("%lu %s\n", result, expr_ready);
-		int i;
-		for(i = 0; i < strLen-1; i++)
-		{
-			expr_2[i] = *(expr_wrong+i);
-		}
+	//  while (fgets(buf, sizeof(buf), fp) != NULL)
+    // {
+	//   strcpy(dtm,buf);//把 buf 所指向的字符串复制到dtm。
+	//   arg = strtok(dtm, " ");// 在第一次分割时，需要指定源字符串
+  	//   if(arg != NULL)
+    //   {
+	// 	sscanf(dtm,"%lu", &result);//sscanf有局限性，遇到空格就结束了，所以只用于读取result
+   	//   	expr_wrong = strtok(NULL, "\0");//往后的调用则将参数s设置成NULL
+	// 	int strLen = strlen(expr_wrong);
+	//     // printf("%lu %s\n", result, expr_ready);
+	// 	int i;
+	// 	for(i = 0; i < strLen-1; i++)
+	// 	{
+	// 		expr_2[i] = *(expr_wrong+i);
+	// 	}
 
-		expr_ready = expr_2;
-		printf("expr = %s\n", expr_ready);
-      	expr(expr_ready,success);
-		memset(expr_2, '\0', sizeof(expr_2));
-      }
-    }
-	  fclose(fp);//关闭数据文件
+	// 	expr_ready = expr_2;
+	// 	printf("expr = %s\n", expr_ready);
+    //   	expr(expr_ready,success);
+	// 	memset(expr_2, '\0', sizeof(expr_2));
+    //   }
+    // }
+	//   fclose(fp);//关闭数据文件
  /********测试表达式求值-end********/
   
   
