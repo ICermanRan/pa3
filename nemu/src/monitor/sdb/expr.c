a6f59a650f8097b3743b8844b517ccea2d2e25cf
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-21 20:22:10 +0800

    > compile NEMU ysyx_22050078 李秋然 Linux ran 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:22:10 up  9:03,  1 user,  load average: 0.75, 0.66, 0.73

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -209,15 +209,12 @@ static bool make_token(char *e) {
       token_addr++;
   }
 
-  // char * substr_end = "\0"; 
-  // strncpy(tokens[token_addr].str,substr_end,2);
-
-  // int c;
-  // for(c = 0; c < token_addr; c++)
-  // {
-  //    printf("outloop： tokens[%d].type = %d ", c, tokens[c].type);
-  //    printf("tokens[%d].str = %s\n", c,tokens[c].str);
-  // }
+  int c;
+  for(c = 0; c < token_addr; c++)
+  {
+     printf("outloop： tokens[%d].type = %d ", c, tokens[c].type);
+     printf("tokens[%d].str = %s\n", c,tokens[c].str);
+  }
 
    token_addrs = token_addr-1;
    value = eval(0,token_addrs);
