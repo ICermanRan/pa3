6c0e3f9b56944d4bd5e102b531448c1928df186c
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-22 19:03:27 +0800

    > compile NEMU ysyx_22050078 李秋然 Linux ran 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:03:27 up 57 min,  1 user,  load average: 0.98, 0.76, 0.62

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -431,9 +431,9 @@ static int eval(int start, int end)  //p=开始位置，q=结束位置
 
   else if (p == q)
    {
-    // printf("2、判断为:It's a number\n"); 
+     printf("2、判断为:It's a number\n"); 
      value_num = atoi(tokens[p].str); 
-    // printf("value_num = %lu\n" , value_num);
+     printf("value_num = %lu\n" , value_num);
      result = value_num;
      return result;
    }
@@ -450,19 +450,23 @@ static int eval(int start, int end)  //p=开始位置，q=结束位置
     
     if((check_parentheses(p+1,q-1) == 1) && (check_surround(p+1,q-1) == false))
     {
-      // printf("去掉外面一层括号后，不再被括号包围且内部括号配对：\n");
+      printf("去掉外面一层括号后，不再被括号包围且内部括号配对：\n");
       result = eval(p + 1, q - 1);
+      printf("result = %lu\n", result);
       return result;
     }
     else if((check_parentheses(p+1,q-1) == 0) && (check_surround(p+1,q-1) == false))
     {
-      //  printf("去掉外面一层括号后，不再被括号包围,但内部括号不配对：\n");
+      printf("去掉外面一层括号后，不再被括号包围,但内部括号不配对：\n");
       //  printf("此时先找主运算符");
       op = main_op(p,q);
       op_type = tokens[op].type;
-      //  printf("在去掉两边括号不对时，就先找主运算符,the position of 主运算符op = %s in the token expression: %d\n", tokens[op].str, op);
+      printf("在去掉两边括号不对时，就先找主运算符,the position of 主运算符op = %s in the token expression: %d\n", tokens[op].str, op);
       val1 = eval(p, op - 1);
       val2 = eval(op + 1, q);
+
+      printf("val1 = %lu\n", val1);
+      printf("val2 = %lu\n", val2);
       switch (op_type) 
       {
       case '+': return result = val1 + val2;
@@ -474,20 +478,23 @@ static int eval(int start, int end)  //p=开始位置，q=结束位置
     }
     else if((check_parentheses(p+1,q-1) == 1) && (check_surround(p+1,q-1) == true))
     {
-      // printf("去掉外面一层括号后，仍被括号包围且内部括号配对：\n");
+      printf("去掉外面一层括号后，仍被括号包围且内部括号配对：\n");
       result = eval(p + 1, q - 1);
+      printf("result = %lu\n", result);
       return result;
     }
   
   }
   else {
-    // printf("针对去掉括号没有问题的时候：\n");
+     printf("针对去掉括号没有问题的时候：\n");
     op = main_op(p,q);
     op_type = tokens[op].type;
     // printf("找主运算符,the position of 主运算符op = %s in the token expression: %d\n", tokens[op].str, op);
     // printf("开始求值\n");
     val1 = eval(p, op - 1);
     val2 = eval(op + 1, q);
+    printf("val1 = %lu\n", val1);
+      printf("val2 = %lu\n", val2);
       switch (op_type) {
       case '+': return result = val1 + val2;
       case '-': return result = val1 - val2;
