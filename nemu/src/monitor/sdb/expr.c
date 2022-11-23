08d5ef207a021a143bd8264ce6e4c75dfaedf0a9
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-23 19:30:23 +0800

    > compile NEMU ysyx_22050078 李秋然 Linux ran 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:30:23 up  6:20,  1 user,  load average: 0.77, 0.62, 0.58

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -442,7 +442,7 @@ static int eval(int start, int end)  //p=开始位置，q=结束位置
   p = start;
   q = end;
   
-  // printf("进入eval,p = %d, q = %d\n", p, q);
+   printf("进入eval,p = %d, q = %d\n", p, q);
 
 
   if (p > q)
@@ -453,14 +453,19 @@ static int eval(int start, int end)  //p=开始位置，q=结束位置
 
   else if (p == q)
    {
-    //  printf("2、判断为:It's a number\n");
+      printf("2、判断为:It's a number\n");
      value_num = atoi(tokens[p].str); 
     //  printf("value_num = %lu\n" , value_num);
      if(tokens[p-1].type == TK_NEG)
-      result = 0-value_num;
+      {
+        result = 0-value_num;
+        printf("value_num = %lu\n" , value_num);
+      }
      else
-      result = value_num;
-     
+     {
+        result = value_num;
+        printf("value_num = %lu\n" , value_num);
+     }
     return result;
    }
 
