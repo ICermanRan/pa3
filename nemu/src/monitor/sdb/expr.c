32b4bc3e953146340d7ca667de05224869f22e93
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-23 18:58:40 +0800

    > compile NEMU ysyx_22050078 李秋然 Linux ran 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:58:40 up  5:48,  1 user,  load average: 1.52, 0.94, 0.64

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -116,8 +116,8 @@ static bool make_token(char *e) {
         int substr_len = pmatch.rm_eo; //存放匹配字符串长度
 
 
-        // Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
-        //     i, rules[i].regex, position, substr_len, substr_len, substr_start);
+         Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
+             i, rules[i].regex, position, substr_len, substr_len, substr_start);
       
 
       //  printf("i = %d, rules[i] = %s\n",  i, rules[i].regex);
