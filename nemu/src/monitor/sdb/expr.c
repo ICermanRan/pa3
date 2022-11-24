5d7974df2d6c1853eca791909e1b321eb0a4f69b
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-24 15:29:26 +0800

    > compile NEMU ysyx_22050078 李秋然 Linux ran 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:29:26 up  2:35,  1 user,  load average: 0.49, 0.50, 0.46

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -464,8 +464,9 @@ static int eval(int start, int end)  //p=开始位置，q=结束位置
   else if((q == p+1) && (tokens[p].type == TK_DEREF) && (tokens[q].type == TK_HEX))
    {
     //判定为指针解引用
+     printf("%s", tokens[q].str);
      long long DEREF_addr = atoi(tokens[q].str);
-      printf("DEREF_addr = %#llx\n", DEREF_addr);
+     printf("DEREF_addr = %#llx\n", DEREF_addr);
      result = vaddr_read(DEREF_addr,4);
      return result;
 
