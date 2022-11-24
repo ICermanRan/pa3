30c0197f76b7a3101b4c1fd0d12d1ad6eb4a1d3d
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-24 22:19:08 +0800

    > compile NEMU ysyx_22050078 李秋然 Linux ran 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:19:08 up  9:25,  1 user,  load average: 0.69, 0.60, 0.64

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -55,8 +55,8 @@ word_t isa_reg_str2val(const char *s, bool *success) {
   GET_ARRAY_LEN(regs,len);
   j = find_string(regs, s, len);
   printf("j = %d\n", j);
-  long int reg_value = cpu.gpr[j];	 
-  printf("reg_value = %ld", reg_value);
-  word_t a=0;
-  return a;
+  word_t reg_value = cpu.gpr[j];	 
+//   printf("reg_value = %ld\n", reg_value);
+//   word_t a=0;
+  return reg_value;
 }
