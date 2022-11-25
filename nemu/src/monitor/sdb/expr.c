f05286deb783ed55fb4bc13e2581ff8cabe5121f
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-25 15:07:19 +0800

    > compile NEMU ysyx_22050078 李秋然 Linux ran 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:07:19 up  2:05,  1 user,  load average: 1.11, 0.61, 0.43

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -53,7 +53,7 @@ static struct rule {
     {"&&", TK_AND},        //and
     {"-", TK_NEG},         //negative
     {"\\*", TK_DEREF},       //指针解引用(dereference)
-    {"[0][xX][0-9a-fA-F]+", TK_HEX}, //hexadecimal-number
+    {"[0x][0-9a-fA-F]+", TK_HEX}, //hexadecimal-number
     {"\\$[\\$]?[a-z0-9]+", TK_REG}
     // {"$[a-z0-9$]{1,3}", TK_REG}
     //  {"\\$", '$'}
