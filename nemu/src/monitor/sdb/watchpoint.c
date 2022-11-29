4e6d024bb2e143fcf77f089ea22d6b7cfcf43851
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-29 20:24:15 +0800

    > compile NEMU ysyx_22050078 李秋然 Linux ran 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:24:15 up  1:32,  1 user,  load average: 0.40, 0.48, 0.49

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -171,10 +171,10 @@ int test_change()
 void print_wp()
 {
   WP *p = head;
-  printf("WP_NO\tEXPR\tDEX_Value\tHEX_Value\n");
+  printf("WP_NO\tEXPR\t\tDEX_Value\tHEX_Value\n");
   while(p != NULL)
   {
-    printf("%d\t%s\t%9lu\t%#010lx\n", p->NO, p->exp, p->old_value, p->old_value);
+    printf("%d\t%11s\t%9lu\t%#010lx\n", p->NO, p->exp, p->old_value, p->old_value);
     p = p->next;
   }
 
