6217cdeb4e951817a639cdaf687901a2f6d35104
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-30 21:02:13 +0800

    > compile NEMU ysyx_22050078 李秋然 Linux ran 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:02:13 up 11:03,  1 user,  load average: 0.61, 0.47, 0.41

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -504,7 +504,7 @@ static int eval(int start, int end)  //p=开始位置，q=结束位置
      s = tokens[p].str + 1;//为了舍弃掉表示读寄存器的$符号
     //  printf("s = %s\n", s);
      word_t value = isa_reg_str2val(s, success);
-    //  printf("valu = %#010lx\n", value);
+      printf("value = %#010lx\n", value);
     //  printf("value = %lu\n", value);
      return result = value;
 
