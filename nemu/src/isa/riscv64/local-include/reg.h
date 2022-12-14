fab5771fd89982119e77afa773be8f05f651fa34
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-14 20:03:34 +0800

    > compile NEMU ysyx_22050078 李秋然 Linux ran 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:03:34 up 18 min,  1 user,  load average: 0.49, 0.38, 0.29

--- nemu/src/isa/riscv64/local-include/reg.h
+++ nemu/src/isa/riscv64/local-include/reg.h
@@ -19,9 +19,9 @@
 #include <common.h>
 
 static inline int check_reg_idx(int idx) {
-  // IFDEF(CONFIG_RT_CHECK, assert(idx >= 0 && idx < 32));
-  IFDEF(CONFIG_RT_CHECK, assert(idx >= -10));
-  printf("idx = %d\n",idx);
+  IFDEF(CONFIG_RT_CHECK, assert(idx >= 0 && idx < 32));
+  // IFDEF(CONFIG_RT_CHECK, assert(idx >= -10));
+  // printf("idx = %d\n",idx);
   return idx;
 }
 
