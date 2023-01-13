9c2f83eaa5788874dd2e751a97e5f55b95f7ac3a
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-13 19:18:23 +0800

    > compile NEMU ysyx_22050078 李秋然 Linux ran 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:18:23 up  1:50,  1 user,  load average: 0.33, 0.35, 0.35

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -7,6 +7,8 @@
 
 static const char num_table[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 
+///* 将字符数字串转换成整数, 并将指针前移 */
+//为什么宏里data*10：因为如果十进制数字有多位，因为是从左往右读，每一位都相当于往左移后，再加上后面的值，最后全部转换完
 #define read_num(fmt,data) {\
 	while(*(fmt)>='0'&&*(fmt)<='9') data = (data) *10 +*(fmt) -'0',++fmt;\
 }
@@ -45,58 +47,84 @@ int printf(const char *fmt, ...) {
  default: d=va_arg(ap,int);break;\
 }
 
+//fmt: %[flags][width][.precision][length]specifier
+/* 使用参数列表发送格式化输出到字符串 */
 int vsprintf(char *out, const char *fmt, va_list ap) {
   int d;
   char c;
-  char *st = out;
+
+  //用于存放转换过程中的字符串
+  char *st = out;         //首先将字符指针指向out(buf)
   char * s;
 
+  //然后扫描格式字符串，对各个格式转换指示进行相应的处理
   while(*fmt)
   {
-    //format specifier?  %[flags][width][.precision][length]
+    // 格式转换指示字符串均以 '%' 开始
+    // 这里从 fmt 格式字符串中扫描 '%', 寻找格式转换字符串的开始
     if(*fmt != '%')
     {
       //if not
+      // 不是格式指示的一般字符均被依次存入st
       *st++ = *fmt++;
       continue;     //如果满足这个if条件，直接进行下一次循环，本次循环不执行后面内容
     }
     else {
       // yes, evaluate it
-      fmt++;
+      fmt++;        //++的作用是跳过第一个%
     }
 
+    //下面取得格式指示字符串中的标志域, 并将标志常量放入 flags 变量中
     int tag;
 
     enum flags_type{
       notype,
-			zero=1,
-			left=2,
-			positive=4,
-			empty=8,
-			sharp=16
+			zero=1,       // 用 '0' 填充
+			left=2,       // 左调整
+			positive=4,   // 显示 '+'
+			empty=8,      // 若为 '+', 则置为空格
+			special=16    // 0x
     };
 
     size_t flags = notype;
     tag = 0;
-    while(!tag){
+    while(!tag)     //如果tag=0，!tag = 1,就会使while无限循环
+    {               //如果字符串里有满足条件的，就会用break终止循环
       switch(*fmt){
-        case '-' : flags |= left; break;
+        //左对齐调整
+        case '-' : flags |= left; break;      // |= ：按位或后赋值
+
+        //放加号
         case '+' : flags |= positive; break;  
+
+        //放空格
         case ' ' : flags|=empty;panic("Not Implemented or Error happens!"); break;
-				case '#' : flags|=sharp;panic("Not Implemented or Error happens!"); break;
-				case '0' : flags|=zero; break;
+				
+        //是特殊转换
+        case '#' : flags|=special;panic("Not Implemented or Error happens!"); break;
+				
+        //要填零(即'0'), 否则是空格
+        case '0' : flags|=zero; break;
         default : tag = 1; break;  
       }
     }
 
-    size_t width = 0;
-    if(*fmt >= '0' && *fmt <= '9')
-      read_num(fmt, width)
-    else if(*fmt == '*')
+    size_t width = 0;     //输出字段宽度
+    
+    //取当前参数字段宽度, 放入width变量中
+    
+    if(*fmt >= '0' && *fmt <= '9')  //如果宽度域中是数值则直接取其为宽度值
+      read_num(fmt, width)    //利用宏将字符数字串转换成整数, 并将指针前移  
+                              //并改写width
+    else if(*fmt == '*')            // 如果宽度域中是字符 '*', 表示下一个参数指定宽度
       {
-        width = va_arg(ap, int);
-        if(width < 0) flags |= left, width = -width;
-        ++fmt;
+         ++fmt;
+        width = va_arg(ap, int);    // 因此调用 va_arg 取宽度值
+        if(width < 0) 
+        {
+          flags |= left;
+          width = -width;
+        }
       };
 
     if(*fmt == '.') panic("Not Support float or Error happens!");
