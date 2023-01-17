1ea66f6af7245be1c52e8e716868e819ff63e2e2
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-17 18:26:38 +0800

    > compile NEMU ysyx_22050078 李秋然 Linux ran 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:26:38 up  5:55,  1 user,  load average: 0.35, 0.35, 0.32

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -162,7 +162,9 @@ void cpu_exec(uint64_t n) {
   g_print_step = (n < MAX_INST_TO_PRINT);   //判断传入的要单步执行的步数，不能大于10
   switch (nemu_state.state) {
     case NEMU_ABORT:
-      
+      #ifdef CONFIG_ITRACE
+        show_iringbuf();
+      #endif
     case NEMU_END: 
       printf("Program execution has ended. To restart the program, exit NEMU and run again.\n");
       return;
@@ -180,9 +182,6 @@ void cpu_exec(uint64_t n) {
     case NEMU_RUNNING: nemu_state.state = NEMU_STOP; break;
     
     case NEMU_ABORT:  case NEMU_END: 
-      #ifdef CONFIG_ITRACE
-        show_iringbuf();
-      #endif
       Log("nemu: %s at pc = " FMT_WORD,
           (nemu_state.state == NEMU_ABORT ? ANSI_FMT("ABORT", ANSI_FG_RED) :
            (nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :
