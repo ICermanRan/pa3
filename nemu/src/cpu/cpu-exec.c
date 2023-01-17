baeb53a0f24757d5eb1f4ea4d528ce38e4c23442
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-17 18:27:58 +0800

    > compile NEMU ysyx_22050078 李秋然 Linux ran 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:27:58 up  5:56,  1 user,  load average: 0.59, 0.42, 0.34

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -148,7 +148,9 @@ static void statistic() {
 
 static inline void all_fail()
 {
-
+   #ifdef CONFIG_ITRACE
+        show_iringbuf();
+      #endif
   isa_reg_display();
 }
 
@@ -162,9 +164,7 @@ void cpu_exec(uint64_t n) {
   g_print_step = (n < MAX_INST_TO_PRINT);   //判断传入的要单步执行的步数，不能大于10
   switch (nemu_state.state) {
     case NEMU_ABORT:
-      #ifdef CONFIG_ITRACE
-        show_iringbuf();
-      #endif
+      all_fail();
     case NEMU_END: 
       printf("Program execution has ended. To restart the program, exit NEMU and run again.\n");
       return;
