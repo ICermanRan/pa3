df63bd39e7a80257c4670b099b58009c17542151
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-17 21:00:01 +0800

    > compile NEMU ysyx_22050078 李秋然 Linux ran 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:00:01 up  8:28,  1 user,  load average: 0.24, 0.24, 0.25

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -142,10 +142,10 @@ void show_iringbuf()
 {
   for(int i = 0; i <= tot; i++)
   {
-    // printf("i = %d\n", i);
-    // if(i == now)
-    //   printf("--> %s\n", iring_buf[i]);
-    // else 
+    printf("i = %d\n", i);
+    if(i == now)
+      printf("--> %s\n", iring_buf[i]);
+    else 
       printf("    %s\n", iring_buf[i]);
   }
 }
