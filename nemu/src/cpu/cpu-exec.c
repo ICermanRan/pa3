567cdc6674944d46c0bd377b44ecedfcf8118547
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-18 14:13:42 +0800

    > compile NEMU ysyx_22050078 李秋然 Linux ran 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:13:42 up 42 min,  1 user,  load average: 0.35, 0.26, 0.20

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -84,6 +84,13 @@ static void exec_once(Decode *s, vaddr_t pc) {
   /*ITRACE*/
 #ifdef CONFIG_ITRACE
   // vaddr_t tmp = pc;
+
+  strcpy(iring_buf[now], s->logbuf);
+  now = (now + 1) % num_of_buf;
+  if(now > tot) 
+  tot = now;   
+
+
   char *p = s->logbuf;
   p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc);  //
   int ilen = s->snpc - s->pc;
@@ -91,7 +98,6 @@ static void exec_once(Decode *s, vaddr_t pc) {
   uint8_t *inst = (uint8_t *)&s->isa.inst.val;
   for (i = ilen - 1; i >= 0; i --) {
     p += snprintf(p, 4, "%02x ", inst[i]);
-    printf("指令: %s\n", p);
   }
   int ilen_max = MUXDEF(CONFIG_ISA_x86, 8, 4);
   int space_len = ilen_max - ilen;
