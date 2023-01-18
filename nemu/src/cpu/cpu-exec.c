1f07cac66e3adde3df675ee1daa01dc5e55ebffa
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-18 16:21:09 +0800

    > compile NEMU ysyx_22050078 李秋然 Linux ran 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:21:09 up  2:50,  1 user,  load average: 0.62, 0.56, 0.44

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -108,9 +108,9 @@ static void exec_once(Decode *s, vaddr_t pc) {
 
   //void disassemble：把指令翻译成反汇编内容(从数字——>字符串)
   void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
-    printf("222 %s\n", s->logbuf);
   disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
       MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst.val, ilen);
+  printf("222 %s\n", s->logbuf);
 #endif
 }
 
