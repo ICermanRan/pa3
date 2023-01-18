198bf1c5eaf5f42093a92794989d4f8d9c940f35
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-18 16:16:47 +0800

    > compile NEMU ysyx_22050078 李秋然 Linux ran 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:16:47 up  2:45,  1 user,  load average: 0.25, 0.41, 0.37

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -99,8 +99,8 @@ static void exec_once(Decode *s, vaddr_t pc) {
     p += snprintf(p, 4, "%02x ", inst[i]);
   } //循环的作用是把
   int ilen_max = MUXDEF(CONFIG_ISA_x86, 8, 4);
-    printf("222 %s\n", s->logbuf);
   int space_len = ilen_max - ilen;
+  printf("222 %s\n", s->logbuf);
   if (space_len < 0) space_len = 0;
   space_len = space_len * 3 + 1;
   memset(p, ' ', space_len);
