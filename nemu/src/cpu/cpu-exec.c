e1f0061252580234ac7bc9daa2af354b12e6c663
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-18 21:04:52 +0800

    > compile NEMU ysyx_22050078 李秋然 Linux ran 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:04:52 up  7:33,  1 user,  load average: 0.25, 0.26, 0.26

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -85,20 +85,20 @@ static void exec_once(Decode *s, vaddr_t pc)
   char *p = s->logbuf;
   // printf("000 %s\n", s->logbuf);
   p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc);  //这里用于把pc:存入s->logbuf,并且去掉了最开始未定义的乱码pc
-  printf("111 %s\n", s->logbuf);
+  // printf("111 %s\n", s->logbuf);
   int ilen = s->snpc - s->pc;
   int i;
   uint8_t *inst = (uint8_t *)&s->isa.inst.val;
-  printf("222 %s\n", s->logbuf);
+  // printf("222 %s\n", s->logbuf);
   for (i = ilen - 1; i >= 0; i --) {
     p += snprintf(p, 4, "%02x ", inst[i]);
   } //循环的作用是把指令中的内存给翻译出来存入s->logbuf
-  printf("333 %s\n", s->logbuf);
+  // printf("333 %s\n", s->logbuf);
   int ilen_max = MUXDEF(CONFIG_ISA_x86, 8, 4);
   int space_len = ilen_max - ilen;
   if (space_len < 0) space_len = 0;
   space_len = space_len * 3 + 1;
-  printf("444 %s\n", s->logbuf);  
+  // printf("444 %s\n", s->logbuf);  
   memset(p, ' ', space_len);  //从循环结束到这一句结束的作用，是把指令内容翻译出来，存入iringbuf
                               //例如addi        sp, sp, -4
   printf("555 %s\n", s->logbuf); 
