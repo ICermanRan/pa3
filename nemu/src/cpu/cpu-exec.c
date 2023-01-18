f2ab41d06969c555566dde520751a220c73618db
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-18 16:17:44 +0800

    > compile NEMU ysyx_22050078 李秋然 Linux ran 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:17:44 up  2:46,  1 user,  load average: 0.50, 0.45, 0.38

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -97,12 +97,12 @@ static void exec_once(Decode *s, vaddr_t pc) {
   uint8_t *inst = (uint8_t *)&s->isa.inst.val;
   for (i = ilen - 1; i >= 0; i --) {
     p += snprintf(p, 4, "%02x ", inst[i]);
-  } //循环的作用是把
+  } //循环的作用是把指令中的内存给翻译出来存入s->logbuf
   int ilen_max = MUXDEF(CONFIG_ISA_x86, 8, 4);
   int space_len = ilen_max - ilen;
   if (space_len < 0) space_len = 0;
-  printf("222 %s\n", s->logbuf);
   space_len = space_len * 3 + 1;
+  printf("222 %s\n", s->logbuf);
   memset(p, ' ', space_len);
   p += space_len;
  
