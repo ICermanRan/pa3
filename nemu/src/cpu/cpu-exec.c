c7ba6d39a5a8f365ed7e8e083adfef4f19e16c10
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-25 20:16:21 +0800

    > compile NEMU ysyx_22050078 李秋然 Linux ran 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:16:21 up  7:41,  1 user,  load average: 0.55, 0.52, 0.44

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -165,10 +165,10 @@ function_info * decode_elf(char* elf_file)
       assert(0);
     }
 
-  // if(!check_elf(fp))  //初步检查是否是elf文件
-  //   assert(0);
+  if(!check_elf(fp))  //初步检查是否是elf文件
+    assert(0);
 
-  fseek(fp, 0L, SEEK_END);//fp移动到elf文件末尾
+  fseek(fp, 0, SEEK_END);//fp移动到elf文件末尾
   int elf_size = ftell(fp);//该函数用于得到文件位置指针fp当前位置相对于文件首的偏移字节数
 
   //copy elf file to char *
