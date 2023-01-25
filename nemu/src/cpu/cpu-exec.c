2ae653d08bd3e2e3092d10ed3c3cbb2900e5744a
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-25 21:54:02 +0800

    > compile NEMU ysyx_22050078 李秋然 Linux ran 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:54:02 up  9:19,  1 user,  load average: 0.56, 0.52, 0.46

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -276,7 +276,7 @@ void ftrace(uint64_t pc, uint32_t inst){
   printf("进入ftrace\n");
   if(inst == 0x00008067)  //对应反汇编中指令ret(实际被扩展为jalr)，是每个函数的结尾
   {
-    // assert(ftrace_fp);
+    assert(ftrace_fp);
     fprintf(ftrace_fp, "%x: %*cret  [%s]\n", (uint32_t)pc, 2*call_times, ' ', find_func_name(cpu.gpr[1]));
     call_times--;
   }
