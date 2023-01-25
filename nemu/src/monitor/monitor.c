5d49cd3fe240db1b78f8c42f30789c4b7f1efb9e
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-25 21:53:16 +0800

    > compile NEMU ysyx_22050078 李秋然 Linux ran 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:53:16 up  9:18,  1 user,  load average: 0.37, 0.46, 0.44

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -120,11 +120,11 @@ static int parse_args(int argc, char *argv[]) {
                // decode elf
                extern function_info* fc;    //修饰符extern用在变量或者函数的声明前，用来说明“此变量/函数是在别处定义的，要在此处引用
                fc = decode_elf(elf_file);
-              //  free(elf_file);
+               free(elf_file);
                // open ftrace log file
                extern char* ftrace_log;
                extern FILE* ftrace_fp;
-                ftrace_fp = fopen(ftrace_log, "w");
+               ftrace_fp = fopen(ftrace_log, "w");
                #endif
                break;
 
