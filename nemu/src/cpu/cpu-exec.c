81f8dbb573c3f54c1c9bae12ad059c74b10cf8ca
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-25 20:22:27 +0800

    > compile NEMU ysyx_22050078 李秋然 Linux ran 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:22:27 up  7:47,  1 user,  load average: 0.45, 0.47, 0.43

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -159,20 +159,15 @@ function_info * decode_elf(char* elf_file)
   //get elf size
   FILE * fp;
   fp = fopen(elf_file, "r");//rb:读方式打开一个二进制文件，不允许写数据，文件必须存在
-  if(fp == NULL)
-    {
-      Log_red("Can not open '%s' ,treated as no elf file.",elf_file);
-      assert(0);
-    }
 
-  if(!check_elf(fp))  //初步检查是否是elf文件
-    assert(0);
+
+  // if(!check_elf(fp))  //初步检查是否是elf文件
+  //   assert(0);
 
   fseek(fp, 0, SEEK_END);//fp移动到elf文件末尾
   int elf_size = ftell(fp);//该函数用于得到文件位置指针fp当前位置相对于文件首的偏移字节数
 
   //copy elf file to char *
-  Log_magenta("copy elf file to char");
   char elf[elf_size];
   fseek(fp, 0, SEEK_SET);//fp移动到elf文件开始
   int ret = fread(&elf, elf_size, 1, fp);//将整个elf文件内容复制到char elf
