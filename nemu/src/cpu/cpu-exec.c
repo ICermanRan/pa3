fca27f5d219b5b2ddefc3656ef22582d25aef08e
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-25 22:39:50 +0800

    > compile NEMU ysyx_22050078 李秋然 Linux ran 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:39:50 up 10:05,  1 user,  load average: 0.77, 0.62, 0.58

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -345,7 +345,7 @@ void ftrace(uint64_t pc, uint32_t inst){
   if(inst == 0x00008067){
     assert(ftrace_fp);
     // fprintf(ftrace_fp, "%x: %*cret  [%s]\n", (uint32_t)pc, 2*call_times, ' ', find_func_name(cpu.gpr[1]));
-    printf("%x: %*cret  [%s]\n", (uint32_t)pc, 2*call_times, ' ',find_func_name(cpu.gpr[1]));
+    printf("%x: %*cret  [%s]\n", (uint32_t)pc,  ' ',find_func_name(cpu.gpr[1]));
     call_times--;
   }
   int fc_index = is_call(pc, inst);
