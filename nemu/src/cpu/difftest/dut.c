d398440ba9c7559e5399f0417b2dc44ba71571e6
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-28 18:11:52 +0800

    > compile NEMU ysyx_22050078 李秋然 Linux ran 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:11:52 up  6:27,  1 user,  load average: 0.47, 0.57, 0.54

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -116,6 +116,7 @@ void difftest_step(vaddr_t pc, vaddr_t npc)
 {
   CPU_state ref_r;
   printf("pc = %lx, npc = %lx\n", pc, npc);
+  printf("skip_dut_nr_inst = %d\n", skip_dut_nr_inst);
   if (skip_dut_nr_inst > 0) 
   {
     ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
