43514e8e672376380c645c908c8eefaf11dae689
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-28 18:43:12 +0800

    > compile NEMU ysyx_22050078 李秋然 Linux ran 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:43:12 up  6:59,  1 user,  load average: 0.85, 0.51, 0.45

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -117,9 +117,11 @@ void difftest_step(vaddr_t pc, vaddr_t npc)
   CPU_state ref_r;
   printf("pc = %lx, npc = %lx\n", pc, npc);
   printf("skip_dut_nr_inst = %d\n", skip_dut_nr_inst);
+
   if (skip_dut_nr_inst > 0) 
   {
     ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
+    printf("进入第一种情况\n");
     if (ref_r.pc == npc) 
     {
       skip_dut_nr_inst = 0;
@@ -134,6 +136,7 @@ void difftest_step(vaddr_t pc, vaddr_t npc)
 
   if (is_skip_ref) 
   {
+    printf("进入第二种情况\n");
     // to skip the checking of an instruction, just copy the reg state to reference design
     ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
     is_skip_ref = false;
