d6606d79e8c2a4fcbd82deebbff164941cdbeb40
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-02 15:57:09 +0800

    > compile NEMU ysyx_22050078 李秋然 Linux ran 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:57:09 up  2:08,  1 user,  load average: 0.60, 0.43, 0.32

---
+++ abstract-machine/scripts/platform/npc.mk
@@ -0,0 +1,22 @@
+AM_SRCS:=  platform/dummy/vme.c \
+           platform/dummy/mpe.c
+
+CFLAGS    += -fdata-sections -ffunction-sections
+LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
+			 --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
+LDFLAGS   += --gc-sections -e _start
+NPCFLAGS  += -l $(shell dirname $(IMAGE).elf)/npc-log.txt 
+
+CFLAGS += -DMAINARGS=\"$(mainargs)\"
+
+.PHONY: $(AM_HOME)/am/src/riscv/npc/trm.c run gdb
+
+image: $(IMAGE).elf
+	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
+	@echo + OBJCOPY "->" $(IMAGE_REL).bin
+	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
+
+run: image
+	$(MAKE) -C $(NPC_HOME) run ARGS="$(NPCFLAGS) -b" IMG=$(IMAGE).bin  
+
+
