b26505d19c4d67cff2d84763c43ef7180c132eaf
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-19 10:52:13 +0800

    > sim RTL ysyx_22050078 李秋然 Linux ran 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:52:13 up 16 min,  1 user,  load average: 0.12, 0.15, 0.16

--- npc/csrc/memory/mem.cpp
+++ npc/csrc/memory/mem.cpp
@@ -33,7 +33,6 @@ uint64_t pmem_read(uint64_t addr, int len)
     case 2: return *(uint16_t *)paddr;
     case 4: return *(uint32_t *)paddr;
     case 8: 
-            printf("wuyule\n");
             #ifdef CONFIG_MTRACE
             Log("MTRACE_read:addr = %lx, data = %lx\n", addr, *(uint64_t *)paddr); 
             #endif
