1ac06a27eadfd0afcdc3eae527cdaf2189d550cb
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-19 10:46:20 +0800

    > sim RTL ysyx_22050078 李秋然 Linux ran 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:46:20 up 10 min,  1 user,  load average: 0.18, 0.23, 0.18

--- npc/csrc/utils/log.cpp
+++ npc/csrc/utils/log.cpp
@@ -9,7 +9,7 @@ void init_log(const char *log_file) {
   log_fp = stdout;
   printf("进入init_log, log_file = %s\n", log_file);
   if (log_file != NULL) {
-    FILE *fp = fopen(log_file, "w");
+    FILE *fp = fopen(log_file, "w+");
     Assert(fp, "Can not open '%s'", log_file);
     log_fp = fp;
   }
