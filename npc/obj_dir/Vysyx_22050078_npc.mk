4faff5c8238b47ee712dc3ee81dddab9dd8ebac1
22050078-Li Qiuran <970571374@qq.com>
2023-02-22 16:03:27 +0800

    临时文件

--- npc/obj_dir/Vysyx_22050078_npc.mk
+++ npc/obj_dir/Vysyx_22050078_npc.mk
@@ -35,24 +35,24 @@ VM_PREFIX = Vysyx_22050078_npc
 VM_MODPREFIX = Vysyx_22050078_npc
 # User CFLAGS (from -CFLAGS on Verilator command line)
 VM_USER_CFLAGS = \
-	-O0 \
 	-I/home/ran/ysyx/ysyx-workbench/npc/csrc/include/ \
 	-I/usr/include/SDL2 \
 	-I./obj_dir \
-	g \
-	-lasan \
 	-I/usr/lib/llvm-14/include \
 	-std=c++14 \
 	-fno-exceptions \
 	-D_GNU_SOURCE \
 	-D__STDC_CONSTANT_MACROS \
 	-D__STDC_LIMIT_MACROS \
+	-fPIE \
+	-Ofast \
+	-fexceptions \
 
 # User LDLIBS (from -LDFLAGS on Verilator command line)
 VM_USER_LDLIBS = \
-	-ldl \
 	-lSDL2 \
-	-lSDL2_image \
+	-lreadline \
+	-ldl \
 	-lLLVM-14 \
 
 # User .cpp files (from .cpp's on Verilator command line)
