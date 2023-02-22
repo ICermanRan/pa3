456a5ba1ae5c8e25c1e601d3a5b574adb3ebfd76
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-22 12:11:31 +0800

    > sim RTL ysyx_22050078 李秋然 Linux ran 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:11:31 up  1:22,  1 user,  load average: 0.61, 0.55, 0.42

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -39,6 +39,6 @@ run: image
 	@echo $(MAKE) -C $(NPC_HOME) run IMG=$(IMAGE).bin 
 	@echo $(MAKE) -C $(NPC_HOME) run ARGS=$(NPCFLAGS)
 	@echo $(MAKE) -C $(NPC_HOME) run IMG=$(IMAGE).bin run NPC_ELF="$(NPC_ELF)" run NPC_LOG="$(NPC_LOG)"
-	@$(MAKE) -C $(NPC_HOME) run IMG=$(IMAGE).bin run NPC_ELF="$(NPCELF)" run NPC_LOG="$(NPCLOG)"
+	@$(MAKE) -C $(NPC_HOME) run IMG=$(IMAGE).bin run NPC_ELF=$(NPCELF) run NPC_LOG=$(NPCLOG)
 
 
